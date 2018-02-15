f6feeb30ecb56f8d789cae7a635eab1001d09313
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2018-02-15 13:06:02 -0500

    Add files via upload

---
+++ Potencia.py
@@ -0,0 +1,10 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def Potencia(n,m):
+	if m==0:
+		return 1;
+	if m>=1:
+		return n*potencia(n,m-1)
+
+	
+>>> 
