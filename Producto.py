930d69819ae053155ad4f925d75dbf19a6e6071a
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2018-02-15 12:37:47 -0500

    Update Producto.py

--- Producto.py
+++ Producto.py
@@ -1,5 +1,3 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> def producto(n,m):
 	if m==0:
 		return 0;
@@ -7,6 +5,3 @@ Type "copyright", "credits" or "license()" for more information.
 		return n;
 	return n+producto(n,m-1)
 
->>> print (producto(4,3))
-12
->>> 
