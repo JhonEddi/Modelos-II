8bef0d847eee7886116289afec1fb345ded25416
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2018-02-15 13:12:33 -0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,14 +1,5 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> def fibonacci (n):
 	if n<=2:
 		return 1;
 	if n>2:
 		return fibonacci(n-1)+fibonacci(n-2)
-
-	
->>> print (fibonacci(7))
-13
->>> print (fibonacci(2))
-1
->>> 
