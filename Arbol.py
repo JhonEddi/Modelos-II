a226337fa7ac78904695a7a237b8b768e78ac38a
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2018-03-22 13:50:05 -0500

    Update Arbol.py

--- Arbol.py
+++ Arbol.py
@@ -1,26 +1,29 @@
-class arbol:
+class Arbol:
 	def __init__(self,valor,izq = None, der = None):
 		self.valor = valor
 		self.izquierda = izq
 		self.derecha = der
-
-	def buscar (valor,arbol):
-		if arbol == None:
+arbol15 = Arbol(10,Arbol(5),Arbol(50,Arbol(30,Arbol(20),Arbol(40))))
+def buscar (valor,arbol):
+	if arbol == None:
 			return False
-		if arbol.valor == valor:
+	if arbol.valor == valor:
 			return True
-		if valor > arbol.valor:
-			return buscar(valor,arbol.derecha)
-		return buscar (valor,arbol.izquierda)
+	if valor > arbol.valor:
+		return buscar(valor,arbol.derecha)
+	return buscar (valor,arbol.izquierda)
 	
-	def inorder (arbol):
-		if arbol == None:
-			return []
-		else: 
-			return inorder(arbol.izquierda)+[arbol.valor]+inorder(arbol.derecha)
+def inorder (arbol):
+	if arbol == None:
+		return []
+	else: 
+		return inorder(arbol.izquierda)+[arbol.valor]+inorder(arbol.derecha)
 	
-	def insertar (valor,arbol):
-		if arbol == []:
-			return 
-		else:
-			return insertar(arbol.izquierda)+[arbol.valor]+inorder(arbol.derecha)
+def insertar (valor,arbol):
+	if arbol == None:
+		return Arbol(valor)
+	if valor < arbol.valor:
+		return Arbol(arbol.valor,insertar(valor,arbol.izquierda),arbol.derecha)
+	if valor > arbol.valor:
+		return Arbol(arbol.valor,arbol.izquierda,insertar(valor,arbol.derecha))
+
